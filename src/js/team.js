e9f6f47cac0b8f3bf31d1b35af00a80df0158262
Danulo <danilyanishevski2001@gmail.com>
2024-03-20 12:59:47 +0200

    team-section

---
+++ src/js/team.js
@@ -0,0 +1,62 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import team from './team.json';
+
+const swiperParams = {
+  modules: [Navigation],
+  breakpoints: {
+    375: { slidesPerView: 2, spaceBetween: 20 },
+    768: { slidesPerView: 3, spaceBetween: 50 },
+    1440: { slidesPerView: 6, spaceBetween: 50 },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+};
+
+new Swiper('.swiper', swiperParams);
+const swiperWrapper = document.querySelector('.swiper-wrapper');
+// const open = document.querySelector('.open');
+const developerSection = document.querySelector('.developer-section');
+const close = document.querySelector('.icon-close');
+
+function toggleModal() {
+  const isMenuOpen = developerSection.classList.toggle('is-open');
+  document.body.style.overflow = isMenuOpen ? 'hidden' : '';
+}
+
+// open.addEventListener('click', toggleModal);
+close.addEventListener('click', toggleModal);
+
+const createMrkpSwiper = () => {
+  const markup = team
+    .map(({ src, userName, developer, url }) => {
+      return `<div class="swiper-slide">
+      <div class="developer-container">
+  <div class="container-img">
+    <div class="box-img">
+      <div class="icon-linkedin">
+        <svg class="linkedin" width="16" height="16">
+          <use href="../img/icons/symbol.svg#icon-linkedin"></use>
+        </svg>
+      </div>
+      <a href="${url}" target="_blank"
+        ><img class="dev-photo" src="${src}" alt="${userName}"
+      /></a>
+    </div>
+  </div>
+  <h3 class="devName">${userName}</h3>
+  <p class="dev">${developer}</p>
+  </div>
+</div>`;
+    })
+    .join();
+  return markup;
+};
+
+swiperWrapper.innerHTML = createMrkpSwiper();
