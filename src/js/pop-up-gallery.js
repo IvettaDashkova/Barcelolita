845ca110ac6009a8c0b2267c71fca9bc686c6943
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-03-23 01:34:11 +0200

    add listener

--- src/js/pop-up-gallery.js
+++ src/js/pop-up-gallery.js
@@ -1,5 +1,11 @@
 import Swiper from 'swiper';
-import { Navigation, Scrollbar, EffectCoverflow } from 'swiper/modules';
+import {
+  Navigation,
+  Scrollbar,
+  EffectCoverflow,
+  Keyboard,
+  Mousewheel,
+} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-coverflow';
@@ -7,25 +13,25 @@ import '../css/layout/pop-up-gallery.css';
 import portfolio from './pop-up-gallery.json';
 
 const swiperParams = {
-  modules: [Navigation, Scrollbar, EffectCoverflow],
+  modules: [Navigation, Scrollbar, EffectCoverflow, Keyboard, Mousewheel],
 
   breakpoints: {
     375: { slidesPerView: 1, spaceBetween: 2 },
     // 768: { slidesPerView: 1 },
     1440: {
       slidesPerView: 3,
-      spaceBetween: 20,
+      spaceBetween: 10,
     },
   },
 
   effect: 'coverflow',
-  slidesPerView: 3,
-  spaceBetween: 20,
+  //   slidesPerView: 3,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
-    depth: 100,
+    depth: 200,
     modifier: 1,
+    scale: 0.7,
     slideShadows: true,
   },
 
@@ -37,7 +43,6 @@ const swiperParams = {
   loop: true,
 
   centeredSlides: true,
-  //   centeredSlidesBounds: true,
 
   keyboard: {
     enabled: true,
@@ -45,9 +50,9 @@ const swiperParams = {
 
   mousewheel: true,
 
-  //   scrollbar: {
-  //     el: '.swiper-scrollbar',
-  //   },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 };
 
 const popUpGallerySlider = sliderData => {
@@ -61,15 +66,42 @@ popUpGallerySlider('photo');
 const wrapper = document.querySelector('.swiper-wrapper');
 const closeBtn = document.querySelector('.pop-up-gallery-icon-close');
 const galleryBackdrop = document.querySelector('.pop-up-gallery-backdrop');
+const portfolioList = document.querySelector('.portfolio-list');
+const popUpGalleryArrowPrev = document.querySelector(
+  '.pop-up-gallery-swiper-arrow-prev'
+);
+const popUpGalleryArrowNext = document.querySelector(
+  '.pop-up-gallery-swiper-arrow-next'
+);
+
+popUpGalleryArrowNext.addEventListener('click', e => {
+  popUpGalleryArrowPrev.style.fill = '#f9f9f9';
+});
+
 closeBtn.addEventListener('click', e => {
   galleryBackdrop.classList.remove('is-open');
 });
 
-function renderPopUpGallery(portfolio) {
-  const galleryCurrent = portfolio.find(
-    currentType => currentType.name === 'LOOKBOOK'
-  );
+portfolioList.addEventListener('click', e => {
+  if (
+    e.target.nodeName === 'IMG' ||
+    e.target.nodeName === 'H3' ||
+    e.target.nodeName === 'P' ||
+    e.target.nodeName === 'LI'
+  ) {
+    let portfolioItemName = e.target.closest('.portfolio-item').dataset.popup;
 
+    console.log(portfolioItemName);
+    galleryBackdrop.classList.add('is-open');
+    renderPopUpGallery(portfolio, portfolioItemName);
+  }
+});
+function renderPopUpGallery(portfolio, portfolioItemName) {
+  const galleryCurrent = portfolio.find(
+    currentType =>
+      currentType.name.toLowerCase() === portfolioItemName.toLowerCase()
+  );
+  //   console.log(galleryCurrent);
   const { name, description, img } = galleryCurrent;
 
   document.querySelector('.pop-up-gallery-title').textContent = name;
@@ -91,5 +123,3 @@ function renderPopUpGallery(portfolio) {
 
   wrapper.innerHTML = markup;
 }
-
-renderPopUpGallery(portfolio);
