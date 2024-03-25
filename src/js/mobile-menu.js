368f68e8c913f92df7f8a97f994d34d2a66c102d
izzwerg <izzwerg.kr@gmail.com>
2024-03-25 23:48:42 +0200

    Update mobile-menu.js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -6,8 +6,23 @@ function mobileMenu() {
   const allSite = document.querySelector('body');
   const menuLinks = document.querySelectorAll('.mobmenu-menu-link');
 
+  history.pushState(
+    null,
+    null,
+    window.top.location.pathname + window.top.location.search
+  );
+  window.addEventListener('popstate', e => {
+    e.preventDefault();
+    closeMenu();
+    history.pushState(
+      null,
+      null,
+      window.top.location.pathname + window.top.location.search
+    );
+  });
+
   function openMenu() {
-    allSite.classList.add('noscroll')
+    allSite.classList.add('noscroll');
     backdropMenu.classList.add('mobmenu-visible');
     const timerId = setTimeout(() => {
       backdropMenu.classList.add('mobmenu-open');
@@ -15,11 +30,11 @@ function mobileMenu() {
     closeButton.addEventListener('click', closeMenu);
     menuLinks.forEach(menuLink => {
       menuLink.addEventListener('click', closeMenu);
-    })
+    });
   }
 
   function closeMenu() {
-    allSite.classList.remove('noscroll')
+    allSite.classList.remove('noscroll');
     backdropMenu.classList.remove('mobmenu-open');
     const timerId = setTimeout(() => {
       backdropMenu.classList.remove('mobmenu-visible');
@@ -27,7 +42,7 @@ function mobileMenu() {
     closeButton.removeEventListener('click', closeMenu);
     menuLinks.forEach(menuLink => {
       menuLink.removeEventListener('click', closeMenu);
-    })
+    });
   }
 }
 
