300f712f9194c506ec696c359ecb8d8d6bf51dc0
Alla10475 <sopinskaya10475@gmail.com>
2024-03-26 10:42:50 +0200

    update hero

--- src/js/open-close-modal.js
+++ src/js/open-close-modal.js
@@ -1,11 +1,28 @@
 const contactMeBtn = document.querySelector('.js-open-modal');
 const backdropModal = document.querySelector('.backdrop-modal-contact-me');
-const contactMeCloseBtn = document.querySelector('.close-btn-contact-me')
+const contactMeCloseBtn = document.querySelector('.close-btn-contact-me');
 
 contactMeBtn.addEventListener('click', () => {
-    backdropModal.classList.remove('is-hidden');
+  backdropModal.classList.remove('is-hidden');
 });
 
 contactMeCloseBtn.addEventListener('click', () => {
-    backdropModal.classList.add('is-hidden');
+  backdropModal.classList.add('is-hidden');
 });
+
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+      backdropModal.classList.add('is-hidden');
+      focusBtn();
+  }
+});
+
+function focusBtn() {
+  const ontactMeBtn = document.getElementById('clickMe');
+
+
+  setTimeout(() => {
+    contactMeBtn.blur();
+  }, 1000);
+}
+
