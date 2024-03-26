82e7de8f50cb82bfe64af1cbca965fc7ea7a73fd
sunny7657 <ksvetlana466@gmail.com>
2024-03-26 21:15:07 +0100

    install izitoast

--- src/js/open-close-modal.js
+++ src/js/open-close-modal.js
@@ -1,15 +1,15 @@
 const contactMeBtn = document.querySelector('.js-open-modal');
 const backdropModal = document.querySelector('.backdrop-modal-contact-me');
 const contactMeCloseBtn = document.querySelector('.close-btn-contact-me');
-const contactMeBtnPrice = document.querySelector('.price-contact-btn')
+const contactMeBtnPrice = document.querySelector('.price-contact-btn');
 
 contactMeBtnPrice.addEventListener('click', () => {
-    backdropModal.classList.remove('is-hidden');
+  backdropModal.classList.remove('is-hidden');
 });
 contactMeBtn.addEventListener('click', () => {
-    backdropModal.classList.remove('is-hidden');
+  backdropModal.classList.remove('is-hidden');
 });
 
 contactMeCloseBtn.addEventListener('click', () => {
-    backdropModal.classList.add('is-hidden');
+  backdropModal.classList.add('is-hidden');
 });
