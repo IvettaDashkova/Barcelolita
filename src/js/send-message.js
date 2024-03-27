13f7a1e7b73997812665c31aa554993d963d1566
sunny7657 <ksvetlana466@gmail.com>
2024-03-27 14:03:57 +0100

    fix send-message logic

--- src/js/send-message.js
+++ src/js/send-message.js
@@ -1,31 +1,45 @@
 // import iziToast from 'izitoast';
 
-const form = document.querySelector('#form');
+function sendMessage() {
+  const form = document.querySelector('#form');
 
-form.addEventListener('submit', async e => {
-  e.preventDefault();
+  form.addEventListener('submit', async e => {
+    e.preventDefault();
 
-  let textInput = document.querySelector('#text');
-  let message = textInput.value;
+    let textInput = document.querySelector('#text');
+    let message = textInput.value;
 
-  const token = import.meta.env.VITE_TELEGRAM_TOKEN;
-  const chat_id = import.meta.env.VITE_CHAT_ID;
+    const token = import.meta.env.VITE_TELEGRAM_TOKEN;
+    const chat_id = import.meta.env.VITE_CHAT_ID;
 
-  const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}`;
+    const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}`;
 
-  try {
-    await fetch(url);
-    console.log('suc');
+    let api = new XMLHttpRequest();
+
+    api.open('GET', url, true);
+    api.send();
+
+    // try {
+    //   await fetch(url);
+    //   console.log('suc');
+    //   iziToast.success({
+    //     title: 'Success',
+    //     message: 'Message sent successfully!',
+    //   });
+    // } catch (error) {
+    //   iziToast.error({
+    //     title: 'Error',
+    //     message: 'Failed to send message. Please try again later.',
+    //   });
+    // }
+
+    textInput.value = '';
     iziToast.success({
       title: 'Success',
       message: 'Message sent successfully!',
+      position: 'topRight',
     });
-  } catch (error) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Failed to send message. Please try again later.',
-    });
-  }
+  });
+}
 
-  textInput.value = '';
-});
+export default sendMessage;
--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import mobileMenu from './js/mobile-menu';
 import langSwitcher from './js/lang-switcher';
 import './js/prices';
 import './js/pop-up-gallery';
-
+import sendMessage from './js/send-message';
 import './js/open-close-modal';
 
 mobileMenu();
 langSwitcher();
+sendMessage();
