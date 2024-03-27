db27aa50fd7cdcc27b1a9d3247fd8ca485cd57a5
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-03-28 00:44:59 +0200

    add arrow back in contact me

--- src/js/open-close-modal.js
+++ src/js/open-close-modal.js
@@ -3,21 +3,46 @@ const backdropModal = document.querySelector('.backdrop-modal-contact-me');
 const contactMeCloseBtn = document.querySelector('.close-btn-contact-me');
 const contactMeBtnPrice = document.querySelector('.price-contact-btn');
 
+history.pushState(
+  null,
+  null,
+  window.top.location.pathname + window.top.location.search
+);
+window.addEventListener('popstate', e => {
+  e.preventDefault();
+  closePopUpContact();
+  history.pushState(
+    null,
+    null,
+    window.top.location.pathname + window.top.location.search
+  );
+});
+
 contactMeBtnPrice.addEventListener('click', () => {
   backdropModal.classList.add('is-open');
+  history.pushState(
+    null,
+    null,
+    window.top.location.pathname + window.top.location.search
+  );
 });
 
 contactMeBtn.addEventListener('click', () => {
   backdropModal.classList.add('is-open');
+  history.pushState(
+    null,
+    null,
+    window.top.location.pathname + window.top.location.search
+  );
 });
 
 contactMeCloseBtn.addEventListener('click', () => {
-  backdropModal.classList.remove('is-open');
+  closePopUpContact();
 });
 
 window.addEventListener('keydown', e => {
   if (e.code === 'Escape') {
-    backdropModal.classList.remove('is-open');
+    closePopUpContact();
     focusBtn();
   }
 });
@@ -27,4 +52,9 @@ function focusBtn() {
     contactMeBtn.blur();
     contactMeBtnPrice.blur();
   }, 1000);
+}
+
+function closePopUpContact() {
+  backdropModal.classList.remove('is-open');
+  bodyScroll.classList.remove('noscroll');
 }
