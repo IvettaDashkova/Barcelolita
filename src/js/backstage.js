67bea1a976eb87ad9eb1a3e7cfc786b2056a6f0f
Nikita <nikitaefron06@gmail.com>
2024-03-28 18:56:40 +0100

    summary (required)

--- src/js/backstage.js
+++ src/js/backstage.js
@@ -1,40 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('video');
-    const videoThumbnail = document.getElementById('video-thumbnail');
-    const playButton = document.getElementById('play-button');
-    let isPlaying = false;
+document.addEventListener('DOMContentLoaded', function () {
+  const video = document.getElementById('video');
+  const videoThumbnail = document.getElementById('video-thumbnail');
+  const playButton = document.getElementById('play-button');
+  let isPlaying = false;
 
-    playButton.addEventListener('click', function() {
-        if (isPlaying) {
-            video.pause();
-            isPlaying = false;
-            videoThumbnail.style.display = 'block';
-            playButton.style.display = 'block';
-        } else {
-            video.play();
-            isPlaying = true;
-            video.style.display = 'block';
-            videoThumbnail.style.display = 'none';
-            playButton.style.display = 'none';
-        }
-    });
+  playButton.addEventListener('click', function () {
+    if (isPlaying) {
+      video.pause();
+      isPlaying = false;
+      videoThumbnail.style.display = 'block';
+      playButton.style.display = 'block';
+    } else {
+      video.play();
+      isPlaying = true;
+      video.style.display = 'block';
+      videoThumbnail.style.display = 'none';
+      playButton.style.display = 'none';
+    }
+  });
 
-    video.addEventListener('click', function() {
-        if (isPlaying) {
-            video.pause();
-            isPlaying = false;
-            videoThumbnail.style.display = 'block';
-            playButton.style.display = 'block';
-        } else {
-            video.play();
-            isPlaying = true;
-            video.style.display = 'block';
-            videoThumbnail.style.display = 'none';
-            playButton.style.display = 'none';
-        }
-    });
+  video.addEventListener('click', function () {
+    if (isPlaying) {
+      video.pause();
+      isPlaying = false;
+      videoThumbnail.style.display = 'block';
+      playButton.style.display = 'block';
+    } else {
+      video.play();
+      isPlaying = true;
+      video.style.display = 'block';
+      videoThumbnail.style.display = 'none';
+      playButton.style.display = 'none';
+    }
+  });
 
-    videoThumbnail.style.display = 'block';
-    playButton.style.display = 'block';
+  videoThumbnail.style.display = 'block';
+  playButton.style.display = 'block';
 });
-
