420f0cec6111c7a08333ee3598afe7eede237040
Nikita <nikitaefron06@gmail.com>
2024-03-28 13:26:37 +0100

    import backstage.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import mobileMenu from './js/mobile-menu';
 import langSwitcher from './js/lang-switcher';
 import './js/prices';
 import './js/pop-up-gallery';
+import './js/backstage';
 
 import './js/open-close-modal';
 
