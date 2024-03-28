73a17302604a40fc1d3440e7506c714cb0e91aa4
Nikita <nikitaefron06@gmail.com>
2024-03-28 23:44:46 +0100

    Update backstage.js

--- src/js/backstage.js
+++ src/js/backstage.js
@@ -1,16 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const videoContainer = document.querySelector('.video-container');
   const video = document.getElementById('video');
   const videoThumbnail = document.getElementById('video-thumbnail');
   const playButton = document.getElementById('play-button');
   let isPlaying = false;
 
   playButton.addEventListener('click', function () {
-    if (isPlaying) {
-      video.pause();
-      isPlaying = false;
-      videoThumbnail.style.display = 'block';
-      playButton.style.display = 'block';
-    } else {
+    if (!isPlaying) {
       video.play();
       isPlaying = true;
       video.style.display = 'block';
@@ -19,20 +15,21 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 
-  video.addEventListener('click', function () {
-    if (isPlaying) {
-      video.pause();
-      isPlaying = false;
-      videoThumbnail.style.display = 'block';
-      playButton.style.display = 'block';
-    } else {
-      video.play();
-      isPlaying = true;
-      video.style.display = 'block';
-      videoThumbnail.style.display = 'none';
-      playButton.style.display = 'none';
-    }
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        if (isPlaying) {
+          video.pause();
+          isPlaying = false;
+          video.currentTime = 0;
+          videoThumbnail.style.display = 'block';
+          playButton.style.display = 'block';
+        }
+      }
+    });
   });
+
+  observer.observe(videoContainer);
 
   videoThumbnail.style.display = 'block';
   playButton.style.display = 'block';
