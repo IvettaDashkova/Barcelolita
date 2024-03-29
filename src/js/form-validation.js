d233f4a3098bf2133dee0e29e9961fa6a8159d4b
sunny7657 <ksvetlana466@gmail.com>
2024-03-29 01:48:37 +0100

    update logic
    Number input logic doest work correctly

--- src/js/form-validation.js
+++ src/js/form-validation.js
@@ -4,16 +4,29 @@ numberInput.addEventListener('input', () => {
   const countryCode = numberInput.value.substring(0, 3);
   console.dir(countryCode);
   if (countryCode.startsWith('+38')) {
-    numberInput.setAttribute('pattern', `^\+\d{2}-\d{3}-\d{3}-\d{2}-\d{2}$`);
     numberInput.setAttribute(
-      'title',
-      'Please enter a valid number like +XX-XXX-XXX-XX-XX'
+      'pattern',
+      '^\\+\\d{2}-\\d{3}-\\d{3}-\\d{2}-\\d{2}$'
+    );
+
+    numberInput.setAttribute(
+      'data-en-title',
+      'Please, enter a valid phone number as shown in the example: +XX-XXX-XXX-XX-XX'
+    );
+
+    numberInput.setAttribute(
+      'data-ua-title',
+      'Будь ласка, введіть дійсний номер телефону відповідно до прикладу: +XX-XXX-XXX-XX-XX'
     );
   } else {
-    numberInput.setAttribute('pattern', `^\+\d{2}-\d{3}-\d{3}-\d{3}$`);
+    numberInput.setAttribute('pattern', '^\\+\\d{2}-\\d{3}-\\d{3}-\\d{3}$');
     numberInput.setAttribute(
-      'title',
-      'Please enter a valid number like +XX-XXX-XXX-XXX'
+      'data-en-title',
+      'Please, enter a valid phone number as shown in the example: +XX-XXX-XXX-XXX'
+    );
+    numberInput.setAttribute(
+      'data-ua-title',
+      'Будь ласка, введіть дійсний номер телефону відповідно до прикладу: +XX-XXX-XXX-XXX'
     );
   }
 });
