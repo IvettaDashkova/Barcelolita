b8bdb256283d11060056afb98de7169adb5d1807
sunny7657 <ksvetlana466@gmail.com>
2024-04-01 18:37:43 +0200

    Update contact-me-tab-control.js

--- src/js/contact-me-tab-control.js
+++ src/js/contact-me-tab-control.js
@@ -5,7 +5,6 @@ const closeBtn = document.querySelector('.close-btn-contact-me');
 contactMeBackdrop.addEventListener('keydown', keydownTabModal);
 
 function keydownTabModal(e) {
-  console.dir(e.target);
   if (e.key === 'Tab') {
     if (e.target === modalLastElem) {
       e.preventDefault();
