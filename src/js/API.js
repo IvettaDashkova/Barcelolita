714e84a1f171675b499041a9c85be3cb4ede9fa0
IvettaDashkova <ivettafsd@gmail.com>
2024-04-05 12:23:15 +0200

    add environments

--- src/js/API.js
+++ src/js/API.js
@@ -1,32 +1,37 @@
-// import { createClient }from '@sanity/client';
+import { createClient } from '@sanity/client';
+import iziToast from 'izitoast';
 
-// const client = createClient({
-//   projectId: 'drd3cmm3',
-//   dataset: 'production',
-//   apiVersion: '2023-03-07', // use a UTC date string
-//   token:
-//     'skj4JRPRcBgK6YMJFLCYhP0IN94299xi24wCpzX2i3PFVAmIwumtnqnOo405tDIKmSYMcLYWoFpIpiGXPdj4k395g75uNxMjyMTIFzxsYgJ0gCLLnYH1Uu9iKwiGiOoCv9NVDb3uWp19fhy9eyVib4CGdyYoXAUrkModKOrimcAvyhZfferG', // or leave blank for unauthenticated usage
-//   useCdn: true, // `false` if you want to ensure fresh data
-// });
+createClient({
+  projectId: import.meta.env.VITE_ADMIN_PROJECT_ID,
+  dataset: import.meta.env.VITE_ADMIN_DATASET,
+  apiVersion: import.meta.env.VITE_ADMIN_API_VERSION,
+  token: import.meta.env.VITE_ADMIN_TOKEN,
+  useCdn: true, 
+});
 
 function fetchPortfolio() {
-    // console.log(first)
-    const qwe = fetch(
-      'https://drd3cmm3.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type%3D%3D%22portfolio%22%5D%20%7C%20order(id%20asc)',
+    const portfolio = fetch(
+      `https://${import.meta.env.VITE_ADMIN_PROJECT_ID}.api.sanity.io/${
+        import.meta.env.VITE_ADMIN_API_VERSION
+      }/data/query/${
+        import.meta.env.VITE_ADMIN_DATASET}?query=*%5B_type%3D%3D%22portfolio%22%5D%20%7C%20order(id%20asc)`,
       {
         method: 'GET',
         headers: {
-          // 'Authorization': `Bearer skj4JRPRcBgK6YMJFLCYhP0IN94299xi24wCpzX2i3PFVAmIwumtnqnOo405tDIKmSYMcLYWoFpIpiGXPdj4k395g75uNxMjyMTIFzxsYgJ0gCLLnYH1Uu9iKwiGiOoCv9NVDb3uWp19fhy9eyVib4CGdyYoXAUrkModKOrimcAvyhZfferG`,
           'Content-Type': 'application/json',
         },
       }
     ).then(response => {
-      // Check if the response is successful (status 200-299)
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+           iziToast.error({
+             title: 'Oops...',
+             message: 'Network response was not ok',
+             backgroundColor: 'var(--accent-color)',
+             titleColor: 'var(--primary-color)',
+             messageColor: 'var(--primary-color)',
+           });
       }
-      // Parse the JSON response
       return response.json();
     });
 }
-qwe();
+fetchPortfolio();
